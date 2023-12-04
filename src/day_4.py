3508edfba1c2ad5ac4cbc332021f679169bb6fee
Mikkel Sørensen <MISO@ATP.DK>
2023-12-04 12:13:50 +0100

    Day 4 done.

---
+++ src/day_4.py
@@ -0,0 +1,38 @@
+def part_1(input_lines):
+    result = 0
+
+    for line in input_lines:
+        split_1 = line.split("|")
+        split_2 = split_1[0].strip().split(":")
+        split_3 = set(map(int, split_2[1].strip().split(None)))
+        split_4 = set(map(int, split_1[1].strip().split(None)))
+
+        winning_nums = split_3.intersection(split_4)
+
+        if winning_nums:
+            result += 2 ** (len(winning_nums) - 1)
+
+    print(result)
+
+
+def part_2(input_lines):
+    winning_card_nums = []
+    pending_cards = []
+
+    for card_num, line in enumerate(input_lines):
+        split_1 = line.split("|")
+        split_2 = split_1[0].strip().split(":")
+        winning_nums = set(split_2[1].strip().split(None)).intersection(
+            set(split_1[1].strip().split(None))
+        )
+
+        pending_cards.append(card_num)
+        winning_card_nums.append(len(winning_nums))
+
+    num_cards = len(pending_cards)
+    while pending_cards != []:
+        card = pending_cards.pop()
+        pending_cards.extend(range(card + 1, card + winning_card_nums[card] + 1))
+        num_cards += winning_card_nums[card]
+
+    print(num_cards)
